47510ecf8a4cb5e09f238f8f9243a76330f9fda2
Tamara <tamara.urbanowicz05@gmail.com>
2024-03-13 07:54:38 +0100

    templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = "<br>".join(["<p>" + q.question_text + "</p>" for q in latest_question_list])
+    return HttpResponse(output)
+
+def counter(request):
+    template = loader.get_template('counter.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
