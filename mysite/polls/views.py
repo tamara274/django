5429d9a8bd07a3ac29ca3a3afadb0741aea38bb3
Tamara <tamara.urbanowicz05@gmail.com>
2024-03-20 07:54:07 +0100

    dfr3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,3 +12,21 @@ def counter(request):
     template = loader.get_template('counter.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
