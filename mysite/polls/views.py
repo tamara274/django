86272e5c9c7b6b819e16b6dbbd7f8e21808cad06
Tamara <tamara.urbanowicz05@gmail.com>
2024-03-20 08:23:08 +0100

    2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -30,3 +32,20 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
