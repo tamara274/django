3d2ae379da95f5dde8b7111b3eb19cfcc4a3eaa1
Tamara <tamara.urbanowicz05@gmail.com>
2024-05-29 08:19:26 +0200

    refactor

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,13 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("counter", views.counter, name="counter"),
-]
-
-
-
-urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,51 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from .models import Question
-
-from django.contrib import admin
-
-from .models import Question
-
-admin.site.register(Question)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = "<br>".join(["<p>" + q.question_text + "</p>" for q in latest_question_list])
-    return HttpResponse(output)
-
-def counter(request):
-    template = loader.get_template('counter.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
